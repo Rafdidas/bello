e0e9746e178614bc03bf790577bab8ce1aa1436a
hue <hue@wisa.co.kr>
2024-02-08 15:15:46 +0900

    the com mesldet\ '#' will be ignored, and an empty message aborts the commit.

--- src/App.js
+++ src/App.js
@@ -5,16 +5,28 @@ import Header from './Js/Header';
 import Main from './Js/Main';
 import { Routes, Route } from 'react-router-dom';
 import ShopList from './Js/ShopList';
+import Detail from './Js/Detail';
 
 const App = () => {
 
   return (
     <div id='wrapper'>
       <Header/>
-        <Routes>
-          <Route path='/' element={ <Main/> } exact />
-          <Route path='/shop' element={ <ShopList/> } exact />
-        </Routes>
+      <Routes>
+        <Route path='/*' element={
+          <Routes>
+            <Route path='/' element={ <Main/> } exact />
+            {/* <Route path='/detail/:id/' element={<Detail />} /> */}
+            <Route path='/detail/:id' element={ <Detail/> } />
+          </Routes>
+        } />
+        <Route path='shop/*' element={
+          <Routes>
+            <Route path='/' element={ <ShopList/> } />
+            <Route path='/detail/:id' element={ <Detail/> } />
+          </Routes>
+        } />
+      </Routes>
       <Footer/>
     </div>
   )
---
+++ src/Js/Detail.js
@@ -0,0 +1,31 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import { Pagination,  } from 'swiper/modules';
+
+const Detail = () => {
+    return(
+        <div id="detail">
+            <div className="detail_img">
+                <Swiper
+                    className="detail_slide"
+                    slidesPerView={1.6}
+                    modules={[ Pagination ]}
+                    pagination={{
+                        type: 'fraction',
+                    }}
+                >
+                    <SwiperSlide>
+                        <img src={process.env.PUBLIC_URL + `../../img/detail0.png`} alt="product_image"/>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                        <img src={process.env.PUBLIC_URL + `../../img/detail1.png`} alt="product_image"/>
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+        </div>
+    )
+}
+
+export default Detail;
